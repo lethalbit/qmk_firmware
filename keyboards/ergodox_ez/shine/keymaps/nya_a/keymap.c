0782a927466067296de18def08702f13dcb4709c
Aki Van Ness <aki@lethalbit.net>
2021-08-14 09:41:27 -0400

    made layer toggled momentary

--- keyboards/ergodox_ez/shine/keymaps/nya_a/keymap.c
+++ keyboards/ergodox_ez/shine/keymaps/nya_a/keymap.c
@@ -41,8 +41,8 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_A] = LAYOUT_ergodox(
     /* LEFT */
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_SALL,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(_S),
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    MO(_F),
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(_S),
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
     KC_LSHF, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,
     KC_LCTL, KC_LGUI, KC_LALT, TG(_N),  TG(_F),
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         KC_SPC,  KC_BSPC, KC_UNDO,
     /* RIGHT */
     KC_TILD, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-    TG(_F),  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    TG(_S),  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_END , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL,
                       TG(_F),  TG(_N),  KC_RALT, KC_RGUI, KC_RCTL,
